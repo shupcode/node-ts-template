6d5466add3d80647a017e5723bad700c89ac0ba7
shupcode <shupcode@gmail.com>
2023-01-18 10:12:04 +0800

    rebuild build dir and clean vscode launch

--- build/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloWorld = void 0;
-async function helloWorld() {
-    console.log("hello-world");
-}
-exports.helloWorld = helloWorld;
-//# sourceMappingURL=app.js.map
